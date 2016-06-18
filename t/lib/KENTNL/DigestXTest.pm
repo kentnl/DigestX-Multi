23aeda947c90b76a4b463dd8bd9e1d51389b6707
Kent Fredric <kentfredric@gmail.com>
2016-06-19 08:40:39 +1200

    Improve testing diagnostics

--- t/lib/KENTNL/DigestXTest.pm
+++ t/lib/KENTNL/DigestXTest.pm
@@ -10,7 +10,7 @@ sub test_algo {
   my ( $class, $label, $algo_gen_sub ) = @_;
 
   my $md = DigestX::Multi->new();
-  $md->add_digester( 'digest-1' => $algo_gen_sub->() );
+  $md->add_digester( $label . 'digest-1' => $algo_gen_sub->() );
   $md->append_bytes("Hello World");
 
   my $hash_1 = $md->digests( { format => 'base64' } );
@@ -21,47 +21,47 @@ sub test_algo {
   $builder->note('Testing digests() is non-destructive');
 
   return
-    unless $builder->ok( exists $hash_1->{'digest-1'}, $label . ': digest-1 exists in digest-hash 1' )
-    and $builder->ok( exists $hash_2->{'digest-1'}, $label . ': digest-1 exists in digest-hash 2' );
+    unless $builder->ok( exists $hash_1->{ $label . 'digest-1' }, $label . ': digest-1 exists in digest-hash 1' )
+    and $builder->ok( exists $hash_2->{ $label . 'digest-1' }, $label . ': digest-1 exists in digest-hash 2' );
 
   return
-    unless $builder->ok( defined $hash_1->{'digest-1'}, $label . ': digest-1 defined in digest-hash 1' )
-    and $builder->ok( defined $hash_2->{'digest-1'}, $label . ': digest-1 defined in digest-hash 2' );
+    unless $builder->ok( defined $hash_1->{ $label . 'digest-1' }, $label . ': digest-1 defined in digest-hash 1' )
+    and $builder->ok( defined $hash_2->{ $label . 'digest-1' }, $label . ': digest-1 defined in digest-hash 2' );
 
-  if ( $hash_1->{'digest-1'} eq $hash_2->{'digest-1'} ) {
+  if ( $hash_1->{ $label . 'digest-1' } eq $hash_2->{ $label . 'digest-1' } ) {
     $builder->ok( 1, $label . ': First digest() matches second digest() for digest-1' );
   }
   else {
     $builder->ok( 0, $label . ': First digest() matches second digest() for digest-1' );
     $builder->diag("Digests do not match for $label");
-    $builder->diag( " - Expected: " . $hash_1->{'digest-1'} );
-    $builder->diag( " - Got: " . $hash_2->{'digest-1'} );
+    $builder->diag( " - Expected: " . $hash_1->{ $label . 'digest-1' } );
+    $builder->diag( " - Got: " . $hash_2->{ $label . 'digest-1' } );
     return;
   }
 
   $builder->note('Testing adding new digesters late in cycle');
 
-  $md->add_digester( 'digest-2', $algo_gen_sub->() );
+  $md->add_digester( $label . 'digest-2', $algo_gen_sub->() );
   $md->append_bytes("Hello World");
 
   my $hash_3 = $md->digests( { format => 'base64' } );
 
   return
-    unless $builder->ok( exists $hash_3->{'digest-1'}, $label . ': digest-1 exists in digest-hash 3' )
-    and $builder->ok( exists $hash_3->{'digest-2'}, $label . ': digest-2 exists in digest-hash 3' );
+    unless $builder->ok( exists $hash_3->{ $label . 'digest-1' }, $label . ': digest-1 exists in digest-hash 3' )
+    and $builder->ok( exists $hash_3->{ $label . 'digest-2' }, $label . ': digest-2 exists in digest-hash 3' );
 
   return
-    unless $builder->ok( defined $hash_3->{'digest-1'}, $label . ': digest-2 defined in digest-hash 3' )
-    and $builder->ok( defined $hash_3->{'digest-2'}, $label . ': digest-2 defined in digest-hash 3' );
+    unless $builder->ok( defined $hash_3->{ $label . 'digest-1' }, $label . ': digest-2 defined in digest-hash 3' )
+    and $builder->ok( defined $hash_3->{ $label . 'digest-2' }, $label . ': digest-2 defined in digest-hash 3' );
 
-  if ( $hash_3->{'digest-1'} ne $hash_3->{'digest-2'} ) {
+  if ( $hash_3->{ $label . 'digest-1' } ne $hash_3->{ $label . 'digest-2' } ) {
     $builder->ok( 1, $label . ': Early digest-1 does not equal later digest-2' );
   }
   else {
     $builder->ok( 0, $label . ': Early digest-1 does not equal later digest-2' );
     $builder->diag("Digests unexpectedly match for $label");
     $builder->diag(" - Expected: Anything else");
-    $builder->diag( " - Got: " . $hash_3->{'digest-1'} );
+    $builder->diag( " - Got: " . $hash_3->{ $label . 'digest-1' } );
     return;
   }
 
